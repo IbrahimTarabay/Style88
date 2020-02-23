de2344f8c8223e6ab218dcd86ccc4605ab47dbf6
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-02-23 17:03:48 +0200

    start shop

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';/*higher order component to avoid prop drilling*/ 
 import './menu-item.scss';
 
-const MenuItem = ({title,imageUrl,size,history,match,linkUrl}) =>(
-    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div className='background-image'
-      style={{backgroundImage: `url(${imageUrl})`}}
-    ></div>{/* we put image in another div to force transition of image inside menu-item with help of overflow:hidden */}
+const MenuItem = ({title,imageUrl,size,history,match,linkUrl}) =>{
+  return (<div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
     <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-   </div>
-);
+  </div>);
+};
 
 export default withRouter(MenuItem);
