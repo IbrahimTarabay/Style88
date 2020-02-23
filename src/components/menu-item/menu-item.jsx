bb75004aa77390197618a71e461b7f915bdd904a
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-02-23 10:26:09 +0200

    comment

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -5,7 +5,7 @@ const MenuItem = ({title,imageUrl,size}) =>(
     <div className={`menu-item ${size}`}>
     <div className='background-image'
       style={{backgroundImage: `url(${imageUrl})`}}
-    ></div>
+    ></div>{/* we put image in another div to force transition of image inside menu-item with help of overflow:hidden */}
     <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
