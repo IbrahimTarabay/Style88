7a463a8de3951bc0bff7941414304b9ff85c7ac5
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-03-03 12:22:21 +0200

    add selectCollectionsForPreview to selectors to convert shop.data.js to array

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -13,6 +13,7 @@ const CollectionPreview = ({title,items}) =>(
          .map(item => (
            <CollectionItem key={item.id} item={item} />
          ))}
+         {/*this is the preview when you click on shop link*/}
       </div>
   </div>
 );
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,12 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+export const selectCollectionsForPreview =createSelector(
+ [selectCollections],
+ collections => Object.keys(collections).map(key => collections[key])
+);
+/*we want to make shop.data.js be an array when we map to show collections in shop*/
+
 /*curry function*/
 export const selectCollection = collectionUrlParam =>/*collectionUrlParam is string like hats*/
  createSelector(
